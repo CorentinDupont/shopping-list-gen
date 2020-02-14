8caa73e5a6e1c72de2ae3d8cffee7fcdae146411
Cureau <remicureau35@gmail.com>
2020-02-14 16:59:15 +0100

    feature create/list item

---
+++ ember-shopping-list/tests/unit/controllers/item/create-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | item/create', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:item/create');
+    assert.ok(controller);
+  });
+});
