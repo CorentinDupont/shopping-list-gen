f39c80ccbf3c197df5aee33c7e4bac8dd57e87cf
CorentinDupont <dupont.corentin44@gmail.com>
2020-03-05 16:36:16 +0100

    try to call /items with mirage

--- ember-shopping-list/mirage/config.js
+++ ember-shopping-list/mirage/config.js
@@ -8,8 +8,8 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  // this.urlPrefix = 'http://localhost:3009';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.namespace = '/';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
@@ -23,7 +23,8 @@ export default function() {
 
     https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
   */
-  debugger;
-  this.get('/items');
-  this.post('/items');
+  // this.get('/items');
+  // this.post('/items');
+
+  this.passthrough('items');
 }
