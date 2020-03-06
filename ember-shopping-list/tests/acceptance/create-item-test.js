a547e6f568e74d2a073b28a2e26451baeca4c81d
CorentinDupont <dupont.corentin44@gmail.com>
2020-03-06 16:18:50 +0100

    Add validation form test for create item

--- ember-shopping-list/tests/acceptance/create-item-test.js
+++ ember-shopping-list/tests/acceptance/create-item-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, fillIn, click, pauseTest, andThen } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -11,6 +11,8 @@ module('Acceptance | item', function(hooks) {
 
     assert.equal(currentURL(), '/item/create');
   });
+
+  // valid item creation
   test('create item', async function(assert) {
     await visit('/item/create');
     assert.equal(currentURL(), '/item/create');
@@ -18,6 +20,27 @@ module('Acceptance | item', function(hooks) {
     await fillIn('input#price', 5);
     await click('input[type=button]')
     assert.equal(currentURL(), '/item');
+  });
+
+  // invalid item creation caused by invalid value in form
+  test('create item', async function(assert) {
+    await visit('/item/create');
+    assert.equal(currentURL(), '/item/create');
+
+    const nameLabelText = this.element.querySelector('label[for=name]').textContent;
+    const priceLabelText = this.element.querySelector('label[for=price]').textContent;
+    await fillIn('input#name', '');
+    await fillIn('input#price', -3);
+    await click('input[type=button]')
+    assert.equal(currentURL(), '/item/create');
+    assert.notEqual(
+      this.element.querySelector('label[for=name]').textContent,
+      nameLabelText,
+    )
+    assert.notEqual(
+      this.element.querySelector('label[for=price]').textContent,
+      priceLabelText,
+    )
   });
 });
 
