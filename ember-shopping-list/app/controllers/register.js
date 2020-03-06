69d2965e1689c914a2b8b370fc54296465ee650a
Nicholas Stock <nick@gembani.com>
2020-03-06 13:53:43 +0100

    to actual problem with integraiton

--- ember-shopping-list/app/controllers/register.js
+++ ember-shopping-list/app/controllers/register.js
@@ -6,14 +6,7 @@ export default class RegisterController extends Controller {
 
     @action
     async accountCreation(){
-        let { identification, password  } = this.getProperties('identification', 'password');
-        // let session = getOwner(this).lookup('service:session');
-        // await session.
-        let App = Application.create();
-        App.user = EmberObject.extend();
-        App.session = EmberObject.create();
-
-        App.register('model:user', App.Person, { singleton: false });
-        App.register('session', App.session, { instantiate: false });
+        let user = this.store.createRecord('user', {username: this.indentification, password: this.password})
+        user.save()
     }
 }
