0a4b27989a5f250ac4701e17080e853f6709c0b9
pierre chene <pierre.chene44@gmail.com>
2020-03-06 11:14:27 +0100

    Fix and add style

--- ember-shopping-list/app/controllers/item/create.js
+++ ember-shopping-list/app/controllers/item/create.js
@@ -1,12 +1,38 @@
 import Controller from '@ember/controller';
 import  { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ItemCreateController extends Controller {
 
+  @tracked
+  error = {
+    name: null,
+    price: null
+  }; 
   @action
   handleCreateItem() {
-    
-    this.model.save();
+    if (this.validate) 
+      this.model.save();
+      this.transitionToRoute('item');
+  }
+  @action
+  validate() {
+    this.error = {
+      name: null,
+      price: null
+    }; 
+    const testPrice = typeof this.model.price === 'number' && Number(this.model.price) !== NaN && this.model.price > 0;
+    const testName = typeof this.model.name === 'string' && !!this.model.name.trim();
+    if (!testPrice) {
+      this.error.price = 'Le prix doit être un nombre et supérieur à 0';
+    }
+    if (!testName) {
+      this.error.name = 'Le nom de l\'item ne peut être vide';
+    }
 
+    if (testPrice && testName) 
+      return true;
+    else
+      return false
   }
 }
