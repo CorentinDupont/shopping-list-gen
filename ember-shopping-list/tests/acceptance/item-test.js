bb8be2d861f767cf3c25e87cfffb06a9ef5c8e21
CorentinDupont <dupont.corentin44@gmail.com>
2020-03-19 13:34:26 +0100

    [fix] authentication (#1)
    Fix test after restricting item routes to authenticated users

--- ember-shopping-list/tests/acceptance/item-test.js
+++ ember-shopping-list/tests/acceptance/item-test.js
@@ -2,12 +2,18 @@ import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-
-
+import { authenticateSession } from 'ember-simple-auth/test-support'
 
 module('Acceptance | item', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+
+  hooks.beforeEach(async function (){
+    await authenticateSession({
+      token: 'abcdDEF',
+      token_type: 'Bearer'
+    });
+  })
 
   test('visiting /item', async function(assert) {
     await visit('/item');
