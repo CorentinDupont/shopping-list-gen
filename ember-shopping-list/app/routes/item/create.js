bd4267e47ca9f5281b5e47ca58946d61c31b3b90
CorentinDupont <dupont.corentin44@gmail.com>
2020-03-19 13:26:42 +0100

    [fix] authentication (#1)
    Re enable protected routes on item and item/create

--- ember-shopping-list/app/routes/item/create.js
+++ ember-shopping-list/app/routes/item/create.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class ItemCreateRoute extends Route/*.extend(AuthenticatedRouteMixin)*/ {
+export default class ItemCreateRoute extends Route.extend(AuthenticatedRouteMixin) {
 
   model(){
     return this.store.createRecord('item')
